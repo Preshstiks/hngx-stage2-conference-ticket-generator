d4c8214e4664175faf27d6c31ca233dd46e8c70a
Davies Precious <preshydave.d5@gmail.com>
2025-02-13 12:57:58 +0100

    updates made

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,3 +1,4 @@
+import { EnvelopeIcon } from "@heroicons/react/24/outline";
 import { ErrorMessage, Field, useFormikContext } from "formik";
 
 interface InputFieldProps {
@@ -40,17 +41,23 @@ const InputField = ({
           <span className="text-white text-[12px] font-robotoRegular ml-2">{`(max ${maxLength} words)`}</span>
         )}
       </label>
-      <Field
-        type={type !== "textarea" ? type : undefined}
-        as={type === "textarea" ? "textarea" : undefined}
-        name={name}
-        rows={rows}
-        maxLength={maxLength}
-        className="w-full resize-none outline-none placeholder:text-white p-3 border border-[#07373F] focus:border-[#24A0B5] rounded-lg text-white"
-        placeholder={placeholder}
-        onChange={handleChange}
-      />
-
+      <div className="relative">
+        <Field
+          type={type !== "textarea" ? type : undefined}
+          as={type === "textarea" ? "textarea" : undefined}
+          name={name}
+          rows={rows}
+          maxLength={maxLength}
+          className={`w-full resize-none outline-none placeholder:text-white p-3 border border-[#07373F] focus:border-[#24A0B5] rounded-lg text-white ${
+            name === "email" ? "pl-10" : ""
+          }`}
+          placeholder={placeholder}
+          onChange={handleChange}
+        />
+        {name === "email" && (
+          <EnvelopeIcon className="h-5 w-5 text-white absolute left-3 top-1/2 -translate-y-1/2" />
+        )}
+      </div>
       <ErrorMessage
         name={name}
         component="div"

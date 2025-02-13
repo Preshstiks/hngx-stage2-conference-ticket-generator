84b69ada10a9ec54fce7078cdeac3d87977b7145
Davies Precious <preshydave.d5@gmail.com>
2025-02-13 13:01:53 +0100

    updates made

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <div className="bg-radial px-6 min-h-screen h-full w-screen">
+        <div className="bg-radial sm:px-6 px-3 min-h-screen h-full w-screen">
           <Navbar />
           <MainPage />
           <div className="flex justify-center pb-8 items-center">
